db6bb3f1ff3023313416aa1f69396429142332b9
Marco Aguilar <marcocaaguilar@gmail.com>
2019-09-26 09:25:23 -0500

    Initial commit

---
+++ Encuesta/Models/Encuesta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Encuesta.Models
+{
+    public class Encuesta
+    {
+    }
+}
---
+++ Encuesta/Models/Utilerias/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Encuesta.Models.Utilerias
+{
+    public class Conexion
+    {
+        static readonly string Server = "Localhost";
+        static readonly string Db = "encuesta";
+        static readonly string User = "";
+        static readonly string Pass = "";
+
+        protected SqlConnection conexion { get; set; }
+
+        protected SqlConnection Conectar()
+        {
+            try
+            {
+                conexion = new SqlConnection("Data Source=" + Server + "; Initial Catalog=" + Db + "; User ID=" + User + ";Password" + Pass + ";Integrated Security=True");
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+    }
+}
